1ea8c11a1bb40db9aa7a9ae48b503cd86f868717
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-04-26 11:14:25 +0200

    added circle-cross switch

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 let fields = [];
 
+let currentShape = 'cross';
 
 function fillShape(id) { //um id zu übergeben
-    fields[id] = 'cross';     //um fields den ID zuweisen zu können
+    if(currentShape == 'cross') {
+        currentShape = 'circle';
+    } else {
+        currentShape = 'cross';
+    }
+    fields[id] = currentShape;     //um fields den ID zuweisen zu können
     console.log(fields);
+    draw();
+}
+
+function draw() {
+    for(let i = 0; i < fields.length; i++) {
+        if(fields[i] == 'circle'){
+            document.getElementById('circle-' + i).classList.remove('d-none');
+        }
+        if(fields[i] == 'cross'){
+            document.getElementById('cross-' + i).classList.remove('d-none');
+        }
+        
+    }
 }
